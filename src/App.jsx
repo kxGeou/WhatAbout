aafa8888982a02dddd6ec887e550f25fd0e8cac5
kxGeou <grzegorzm.work@gmail.com>
2025-04-16 22:23:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 function App() {
   return (
     <main>    
-      <Header type="couple"></Header>
+      <Header type="Friends"></Header>
     </main>
   )
 }
